e13a0fc0522d15a445cc29e9759cbbb5ebd0636d
Tim <ti_mur@mail.ru>
2019-11-26 20:14:31 +0300

    BD + typos

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,10 +10,18 @@ const router = new Router().prefix('/auth');
 router.post('/register', async (ctx) => {
     const {name, email, password} = ctx.request.body;
     const user = await User.findOne({email});
+    if (user) {
+        ctx.throw(400, 'Email already exits')
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    await new User({email, name, password: hash}).save();
+    ctx.status = 201
 
 });
 
-router.post('/login', async (ctx) => {});
+router.post('/login', async (ctx) => {
+});
 
 module.exports = router.routes();
 
